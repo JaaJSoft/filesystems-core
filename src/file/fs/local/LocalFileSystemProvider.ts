bdda0b44613cecf6f851145b16078282cbc132fc
pchopinet <pierre.chopinet97@gmail.com>
2021-12-29 22:53:38 +0100

    parse

--- src/file/fs/local/LocalFileSystemProvider.ts
+++ src/file/fs/local/LocalFileSystemProvider.ts
@@ -2,13 +2,14 @@ import {FileSystemProvider} from "../../spi/FileSystemProvider";
 import {FileSystem} from "../../FileSystem";
 import {Path} from "../../Path";
 import {LocalFileSystem} from "./LocalFileSystem";
+import * as os from "os";
 
 export class LocalFileSystemProvider extends FileSystemProvider {
     private readonly theFileSystem: LocalFileSystem;
 
     constructor() {
         super();
-        this.theFileSystem = new LocalFileSystem(this, "/"); // TODO user dir
+        this.theFileSystem = new LocalFileSystem(this, os.homedir());
     }
 
     getTheFileSystem(): LocalFileSystem {
