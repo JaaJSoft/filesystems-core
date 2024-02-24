31c3ffd22e0e2db8ac732461815478d640fdd327
Pierre Chopinet <pierre.chopinet97@gmail.com>
2024-02-24 12:19:43 +0100

    new version

--- src/file/Watchable.ts
+++ src/file/Watchable.ts
@@ -1,3 +1,7 @@
-export interface Watchable {
+import {WatchKey} from "./WatchKey";
+import {WatchService} from "./WatchService";
+import {WatchEventKind, WatchEventModifier} from "./WatchEvent";
 
+export interface Watchable {
+    register(watcher: WatchService, events: WatchEventKind<unknown>[], modifier?: WatchEventModifier[]): WatchKey;
 }
